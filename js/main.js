6a3a66ee70db794bbe4dd21d369c3969e93e5c8b
eligal89 <123586383+eligal89@users.noreply.github.com>
2023-01-26 21:05:11 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,234 +1,182 @@
 'use strict'
 
 const FLAG = '🚩'
-const MINES = '👻' // will change latter
+const MINE = '💣'
+
+var gTimer = false;
 
 
-var gNumOfcells
-var gMines
+
+// var gElMinutes = document.getElementById("min");
+// var gElSeconds = document.getElementById("sec");
+// var gIntervalId;
+
+var gNumberOfMinesInGame
+var gTotalCells
+
 var gBoard
 var gLevel
 var gGame
 
 
-var gElTd
-
-
-
 function onInit() {
-    //console.log('im in');
-    gBoard = {// not sure of how much i need that
-        minesAroundCount: 4,
-        isShowm: true,
-        isMine: false,
-        isMarked: true
-    }
     gLevel = {
-        SIZE: 4,
+        SIZE: 16,
         MINES: 2
     }
     gGame = {
+        minesAround:0,
         isOn: false,
         shownCount: 0,
         markedCount: 0,
         secsPassed: 0
     }
-    gBoard = buildBoard()
+    gBoard = buildBoard(gLevel.SIZE, gLevel.MINES)
+    //console.table(gBoard)
     renderBoard(gBoard)
-
+    //  console.table(gBoard)
 }
 
 //based on the num of mines and total cells this function will build 1: arr of mines; 2: arr of safe cells; 3: arr that combine the 2 of them, shuffle it  and return an array of size**
-// basicly this fun will create the modal 
-function creatingGame(size) {
-    gMines = 2
-    gNumOfcells = 16
+// basicly this fun will create the modal in 1D array
+function creatingGame(gTotalCells, gNumberOfMinesInGame) {
+    var gNumberOfMinesInGame
+    var gTotalCells
     var mines = []
     var safeCells = []
-    for (var i = 0; i < gMines; i++) {
-        mines.push({ id: i, type: "mine", isShowm: true, isMarked: true })
-        // console.log(mines);
 
+    for (var i = 0; i < gNumberOfMinesInGame; i++) {
+        mines.push({ id: i, type: "mine", isShown: false, isMarked: false })
+        // console.log(mines);
     }
-    for (var i = 0; i < gNumOfcells - gMines; i++) {
-        safeCells.push({ id: i + 2, type: "safe", isShowm: true, isMarked: true })
-        // console.log(safeCells);
+    for (var i = 0; i < gTotalCells - gNumberOfMinesInGame; i++) {
+        safeCells.push({ id: i + 2, type: "safe", isShown: false, isMarked: false })
+        //console.log(safeCells);
     }
     var combinemineAndSafe = [...mines, ...safeCells];
     combinemineAndSafe.sort(() => Math.random() - 0.5);
     // console.log(combinemineAndSafe);
-    return combinemineAndSafe
-
+    return (combinemineAndSafe)
 }
-// this function will take combined arr from creatingGame(size) and build a board based on the arr above 
-function buildBoard(squareCells) {
-    var combinemineAndSafe = creatingGame()
-    //console.log(combinemineAndSafe);
-    squareCells = 4
+
+// this function will take combined arr from creatingGame(size) and build a board based on the arr above. this is the modal in 2D array
+function buildBoard(gTotalCells, numOfTotalMines) {
+    var combinemineAndSafe = creatingGame(gTotalCells, numOfTotalMines)
+    var squareRott = (combinemineAndSafe.length ** 0.5)
+
     const board = []
     var counter = 0
-    for (var i = 0; i < squareCells; i++) {
+    for (var i = 0; i < squareRott; i++) {
         board.push([])
-        for (var j = 0; j < squareCells; j++) {
+        for (var j = 0; j < squareRott; j++) {
             var cell = combinemineAndSafe[counter]
-            //console.log(cell.type);
             board[i][j] = cell
             counter++
         }
-
     }
-    for (var i = 0; i < squareCells; i++) {
-        for (var j = 0; j < squareCells; j++) {
-            //console.log('you enterd to line:',i, 'and row:' ,j);
-            // var minesCount =  countMinesNeighbors(i,j,board)
-        }
-    }
-    // console.table(board);
+    renderBoard(board)
+    console.log(board);
     return board
-}
-
-function renderBoard(board) {
-    //console.log('im in');
-    var strHTML = ''
-    for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>'
-        for (var j = 0; j < board[0].length; j++) {
-            var cellIdx = board[i][j].id
-            //console.log(cellIdx);
-            console.log(board[i][j].type);
-            var cellType = board[i][j].type
-            var minesCount = countMinesNeighbors(i, j, board)
-            //var currCell = board[i][j]
-            strHTML += `<td  class="${cellType}" 
-                data-i="${i}" data-j="${j}"
-                onclick="onCellClicked(this)">${minesCount}</td>`
-        }
-        strHTML += '</tr>\n'
-    }
-    // console.log(strHTML)
-    var elBoard = document.querySelector('.board')
-    elBoard.innerHTML = strHTML
-}
-
-
-//for now (!!!) this function will take each cell (including mines) and check how many mines there are surrounding it
-function countMinesNeighbors(cellI, cellJ, board) {
-    console.log('im in');
-    console.log(cellI, cellJ, board);
-    var minesCount = 0
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        console.log('hey');
-        
-        if (i < 0 || i >= board.length) continue
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            console.log('HEY');
-            
-            if (j < 0 || j >= board[i].length) continue
-            if (i === cellI && j === cellJ) continue
-            if (board[i][j].type === 'mine') minesCount++
-            // ?!?!?!?!? how come its seems like ok but not as expected from the log???????????????????
-            console.log(i, j, board[i][j].type === 'mine');
-        }
-    }
-    console.log(minesCount);
-    return minesCount
 }
 
 // firts need to define if there wsa a left or right click 
 // after that - if it was left click there are few options. right click will mark a flag. 
-function onCellClicked(elBtm) {
-    console.log(elBtm);
-    
-    if (elBtm.classList.contains('mine')) {
-        console.log('i am a mine');
-        endGame('lossing')
+function onCellClicked(elBtn, ev, cellI, cellJ) {
+    var elMarkedCell = document.querySelector('.marked-cells')
+    if (ev.button === 0) {
+        if (elBtn.classList.contains('mine')) {
+            elBtn.innerHTML = MINE
+            alert('game over')
+            endGame('lossing')
+        }
+         if (!gBoard[cellI][cellJ].isShown && !elBtn.classList.contains('mine')){
+            gGame.shownCount++
+            elMarkedCell.innerHTML = gGame.shownCount
+            elBtn.innerHTML = ev.target.attributes[2].textContent
+            gBoard[cellI][cellJ].isShown = true
+            console.log(gBoard[cellI][cellJ].isShown)
+            if (ev.target.attributes[2].textContent=== '0' ){
+            expandingAround(cellI, cellJ)
+            }
+        }
     }
-    else if (elBtm.innerHTML === '0') {
-        console.log ('no neighbors')
-    }
-    else {
-        console.log('last case');
-        
-    }
-    // console.log(ev.target);
-    // console.log(ev.srcElement);
-
-    // var elTd = ev.target;
-    // console.log(elTd);
-
-    //     if (elTd.classList.contains('mine')) {
-    //         elTd.innerHTML = MINES;
-    //     }
 }
 
-//function endGame(){}
-//     var elTdSafe = document.querySelector(".safe")
-//     var elTdmine = document.querySelector('.mine')
+//handeling left click 
+function cellMarked(elCell, cellI, cellJ) {
+    // gIntervalId = setInterval(timer, 1000);
+    // gTimer = true;
+    // gGame.isOn = true;
+    var elFlag = document.querySelector('.flag')
+    elFlag.innerText = gGame.markedCount
 
+    if (gBoard[cellI][cellJ].isShown) {
+        console.log('it show i return');
+        return
+    }
 
+    if (gBoard[cellI][cellJ].isMarked != true) {
+        // console.log('im in');
+        gBoard[cellI][cellJ].isMarked = true
+        //console.log(gBoard[1][1].isMarked);
+        elCell.innerHTML = FLAG
+        gGame.markedCount++
+        elFlag.innerText = gGame.markedCount
+    }
+    else {
+        gBoard[cellI][cellJ].isMarked = false
+        elCell.innerHTML = 'x'
+        gGame.markedCount--
+        elFlag.innerText = gGame.markedCount
+    }
+    // console.log(gBoard[cellI][cellJ])
+    // console.log(gBoard[cellI][cellJ].isMarked);
+    // console.log('im in');
+    // console.log(gBoard[cellI][cellJ].isMarked)
+
+    // console.log(gBoard[cellI][cellJ].isMarked)  
+    if (gGame.shownCount === (gTotalCells - gNumberOfMinesInGame) && (gGame.markedCount === numOfTotalMines)) {
+        endGame('winning') // not working yet
+    }
+}
+// if (gBoard[cellI][cellJ].isMarked = true) {
+//     // console.log('im in');
+//     gBoard[cellI][cellJ].isMarked = false
+//     gGame.shownCount--
+//     elCell.innerHTML = 'x'
+
+//switch cases to win or lose
 function endGame(scenario) {
     switch (scenario) {
         case 'lossing': {
-            alert('you lose')
-            break
         }
-        case 'winning': { }
-
+        case 'winning': {
+            console.log('you won!');
+            
+        }
     }
 }
 
-//     clearInterval(gTimerInterval)
-//     //timer()
-// }
-
-// function choseDifficulty(numOfCells) {
-//     console.log('1');
-
-//     switch (numOfCells) {
-//         case 4: gNumOfcells = 16
-//             gMines = 2
-//             totalRows = 4
-//             break;
-//         case 8: gNumOfcells = 64
-//             gMines = 12
-//             totalRows = 8
-//             break;
-//         case 12: gNumOfcells = 124
-//             gMines = 30
-//             totalRows = 12
-//             break;
-//     }
-//     gBoard = createBoard()
-//     renderBoard(gBoard)
-//     console.log('the number of cells will be:', gNumOfcells, 'and total mines', gMines);
-//     creatingGame(gNumOfcells, gMines)
-// }
+function restartGame (){
+    onInit()
+}
 
 
-
-// // //     if (gGameInterval) {
-// // //         clearInterval(gGameInterval)
-// // //         gGameInterval = null
-// // //         elBtn.innerText = 'Start'
-// // //     } else {
-// // //         gGameInterval = setInterval(play, GAME_FREQ)
-// // //         elBtn.innerText = 'Stop'
-// // //     }
-// // // }
-
-// // function play() {
-// //     gBoard = runGeneration(gBoard)
-// //     renderBoard(gBoard)
-// // }
-
-
-
-
-
-
-
-
-
-
-
-
+//this function will be set if you land on a zero cell. still haing problem to understend why undif; maybe duo to string vs num?
+    function expandingAround(cellI, cellJ){ //still haing a problem of undifaind
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= gBoard.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= gBoard[0].length) continue;
+            if (i === cellI && j === cellJ) continue;
+            var elTd = document.getElementById(`${i},${j}`);
+            elTd.innerText = gBoard[i][j].minesAround;
+            if (gBoard[i][j].isShown = false) {
+                gBoard[i][j].isShown = true;
+                gGame.shownCount++;
+                var elMarkedCell = document.querySelector('.marked-cells');
+                elMarkedCell.innerText = gGame.shownCount
+            }
+        }
+    }
+}
--- js/util.js
+++ js/util.js
@@ -1,1 +1,41 @@
- 
+function renderBoard(board) {
+    var strHTML = ''
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+        for (var j = 0; j < board[0].length; j++) {
+            var cellIdx = board[i][j].id
+            //console.log(cellIdx);
+            var cell = 'x'
+           // console.log(board[i][j].type);
+            var cellType = board[i][j].type
+            var minesCount = countMinesNeighbors(i, j, board)
+            //var currCell = board[i][j]
+            strHTML += `<td id=${i},${j} class="${cellType}" cell-value="${minesCount}" data-i="${i}" date-j="${j}"
+            oncontextmenu="cellMarked(this,${i},${j})"
+             id"${minesCount}" 
+                onclick="onCellClicked(this, event, ${i}, ${j})">${cell}</td>`
+        }
+        strHTML += '</tr>\n'
+    }
+    var elBoard = document.querySelector('.board')
+    console.log(elBoard);
+    elBoard.innerHTML = strHTML
+}
+
+
+//for now (!!!) this function will take each cell (including mines) and check how many mines there are surrounding it
+function countMinesNeighbors(cellI, cellJ, board) {
+   // console.log('im in');
+   // console.log(cellI, cellJ, board);
+    var minesCount = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= board[i].length) continue
+            if (i === cellI && j === cellJ) continue
+            if (board[i][j].type === 'mine') minesCount++
+        }
+    }
+   // console.log(minesCount);
+    return minesCount
+}
